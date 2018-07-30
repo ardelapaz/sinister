86feae156c3586d93820d4d06d82ef80372f32cc
Andrew Delapaz <ardelapaz@yahoo.com>
2018-07-30 16:00:10 -0400

    Initial project with little CSS just to get the general layout

---
+++ src/components/view.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class view extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <p>"Hello world"</p>
+            </div>
+        )
+    }
+}
+
+export default view;
